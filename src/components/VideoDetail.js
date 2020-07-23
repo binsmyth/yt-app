340f0025a4b3cfedfcf91e9d87a574ee8137a92c
Vineet Man Singh <binsmyth@gmail.com>
2020-07-24 02:56:56 +1000

    change iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,7 +7,7 @@ const VideoDetail = ({ video }) => {
     const videoSrc = `https:\\www.youtube.com/embed/${video.id.videoId}`;
     return (
         <div>
-            <iframe title="video player" width="560" height="315" src={videoSrc} />
+            <iframe title="video player" width="560" height="315" src={videoSrc} ></iframe>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
